a581bfa15f2c30ee5be0780f68c3ea36509ac616
LakshmiP10 <lakshmi.uitech@gmail.com>
2024-11-15 14:18:46 +0530

    added app2.js

---
+++ app2.js
@@ -0,0 +1,20 @@
+//log, info, warn, error, table
+//log
+console.log('I am from log message');
+// info
+console.info('I am from info message');
+//warn
+console.warn('I am from warn message');
+// error
+console.error('I am from error message');
+//table
+let student = {
+    id: 1,
+    name: 'Lakshmi',
+    age: '25',
+    course: 'MCA'
+};
+
+console.table(student);
+
+
